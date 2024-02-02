b9b457f839cfdf34978f653eb66415345ecef2db
Ramachandran2897 <ramachandran2897@gmail.com>
2024-02-03 01:09:13 +0530

    added new lines

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <h1 style={{display: 'flex',justifyContent:'center', alignItems:'center',}}>
-      Welcome Ramachandran your first deploy is ready 
+      Welcome Ramachandran your first deploy is ready to kick start
     </h1>
   );
 }
