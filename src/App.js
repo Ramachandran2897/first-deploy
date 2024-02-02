d8123d7169ba60107e287f2c0b139605d3572eed
Ramachandran2897 <ramachandran2897@gmail.com>
2024-02-03 00:27:31 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <h1 style={{display: 'flex',justifyContent:'center', alignItems:'center',}}>
+      Welcome Ramachandran your first deploy is ready 
+    </h1>
+  );
+}
+
+export default App;
